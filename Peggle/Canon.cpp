8536e0a553d65ca8dd4f33538295ba33999fc4fb
PAG <pierre.alexandre.garneau.pag@gmail.com>
2017-07-10 00:30:41 -0400

    canon

--- Peggle/Canon.cpp
+++ Peggle/Canon.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 Canon::Canon()
-	: center( 0.0f, .0f, .0f)
+	: center( 9.0f, 9.0f, .0f)
 	, position(.0f, .0f, .0f)
 	, direction(.0f, .0f, .0f)
 	, speed(0)
@@ -26,10 +26,10 @@ void Canon::Update()
 	Input* i = gD3DApp->GetInput();
 	POINT mousePos = i->GetMousePos();
 	//GetCursorPos(&mousePos);
-	//D3DXVECTOR3 vMousePos(mousePos.x, mousePos.y, 0.f); //- gD3DApp->GetResolutionW() / 2, mousePos.y - gD3DApp->GetResolutionH() / 2, 0.f);
-	//D3DXVECTOR3 dir = vMousePos - position;
-	//D3DXVec3Normalize(&dir, &dir);
-	//angle = (atan2(dir.y, dir.x));
+	D3DXVECTOR3 vMousePos(mousePos.x, mousePos.y, 0.f); //- gD3DApp->GetResolutionW() / 2, mousePos.y - gD3DApp->GetResolutionH() / 2, 0.f);
+	D3DXVECTOR3 dir = vMousePos - position;
+	D3DXVec3Normalize(&dir, &dir);
+	angle = (atan2(dir.y, dir.x));
 
 	
 	//SetRotationZ(atan2(dir.y, dir.x) - 90 * DEG_TO_RAD);
@@ -43,28 +43,16 @@ void Canon::Update()
 
 void Canon::Draw(ID3DXSprite* spriteBatch)
 {
-
-	//D3DXVECTOR3 rotEuler = { 0.0f, 0.0f, angle };//D3DX_PI / 4 };
-	//D3DXMATRIX transMatrix;
-	////D3DXMatrixIdentity(&rotMatrix);
-	//D3DXMatrixRotationZ(&rotMatrix, D3DX_PI / 2);
-	//HR(spriteBatch->SetTransform(&rotMatrix));
-	////D3DXMatrixRotationYawPitchRoll(&rotMatrix, rotEuler.x, rotEuler.y, rotEuler.z);
-	////D3DXMatrixTranslation(&rotMatrix, 0, -100, 0);
-	
-	////HR(spriteBatch->SetTransform(&transMatrix));
-
-	D3DXMATRIX rotMatrix;
-	//D3DXMatrixRotationZ(&rotMatrix, angle);
-	//D3DXMatrixRotationZ(&rotMatrix, angle);
-	//D3DXMatrixTranslation(&rotMatrix, 0, -100, 0);
-	D3DXMatrixTranslation(&rotMatrix, 0, -100, 0);
-	HR(spriteBatch->SetTransform(&rotMatrix));
+	D3DXMATRIX transfomMatrix;
+	D3DXVECTOR2 rotationCenter(0, 0);
+	D3DXVECTOR2 translation(0, -300);
+	D3DXMatrixAffineTransformation2D(&transfomMatrix, 1, &rotationCenter, angle, &translation);
+	HR(spriteBatch->SetTransform(&transfomMatrix));
 	HR(spriteBatch->Draw(texture, 0, &center, &position, D3DCOLOR_XRGB(255, 255, 255)));
 
 	//// retore rotation
-	D3DXMatrixIdentity(&rotMatrix);
-	HR(spriteBatch->SetTransform(&rotMatrix));
+	D3DXMatrixIdentity(&transfomMatrix);
+	HR(spriteBatch->SetTransform(&transfomMatrix));
 
 	HR(spriteBatch->Flush());
 }
